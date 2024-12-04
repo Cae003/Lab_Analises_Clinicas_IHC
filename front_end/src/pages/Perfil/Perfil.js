6aa64ca8a5853f1c6c785527ad297277195201c3
caetanojose <caetanojab146@gmail.com>
2024-12-04 09:41:00 -0300

    Carregando Login

--- front_end/src/pages/Perfil/Perfil.js
+++ front_end/src/pages/Perfil/Perfil.js
@@ -1,4 +1,4 @@
-import React, { useContext, useEffect } from 'react';
+import React, { useContext, useEffect, useState } from 'react';
 import "./Perfil.css";
 import Header from '../../components/Header/header';
 import Footer from '../../components/Footer/footer';
@@ -7,6 +7,7 @@ import { useNavigate } from 'react-router-dom';
 import { UserContext } from '../../stores/UserStore';
 
 const Perfil = () => {
+  const [isLoading, setIsLoading] = useState(true);
   const {userData, fetchUserData} = useContext(UserContext);
   // console.log(userData);
   const navigate = useNavigate();
@@ -14,6 +15,7 @@ const Perfil = () => {
   useEffect(() => {
     if (userData.userRegister) {
       fetchUserData(userData.userRegister);
+      setIsLoading(false);
     }
   }, [userData.userRegister, fetchUserData]);
 
@@ -32,23 +34,23 @@ const Perfil = () => {
           <fieldset id="DadosPessoais" className="blocoDados">
             <div className="form-group">
               <label htmlFor="nome">Seu Nome Completo: </label>
-              <input disabled id="nome" type="text" value={userData.nome || "Não Existe"} />
+              <input disabled id="nome" type="text" value={isLoading ? "Carregando..." : userData.nome || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="nascimento">Data de Nascimento: </label>
-              <input disabled id="nascimento" type="text" value={userData.datanasc || "Não Existe"} />
+              <input disabled id="nascimento" type="text" value={isLoading ? "Carregando..." : userData.datanasc || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="cpf">CPF: </label>
-              <input disabled id="cpf" type="text" value={userData.cpf || "Não Existe"} />
+              <input disabled id="cpf" type="text" value={isLoading ? "Carregando..." : userData.cpf || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="matricula">RU (Registro do Usuário): </label>
-              <input disabled id="matricula" maxLength="6" type="text" value={userData.userRegister || "Não Existe"} />
+              <input disabled id="matricula" maxLength="6" type="text" value={isLoading ? "Carregando..." : userData.userRegister || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="senha">Senha: </label>
-              <input disabled id="senha" type="text" value={userData.password || "Não Existe"} />
+              <input disabled id="senha" type="text" value={isLoading ? "Carregando..." : userData.password || "Não Existe"} />
             </div>
             <div className="form-group">
               <label>Sexo Biológico: </label>
@@ -64,34 +66,34 @@ const Perfil = () => {
           <fieldset id="DadosEndereco" className="blocoDados">
             <div className="form-group">
               <label htmlFor="endereco">Endereço: </label>
-              <input disabled id="endereco" type="text" value={userData.endereco || "Não Existe"} />
+              <input disabled id="endereco" type="text" value={isLoading ? "Carregando..." : userData.endereco || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="cep">CEP: </label>
-              <input disabled id="cep" type="text" value={userData.cep || "Não Existe"} />
+              <input disabled id="cep" type="text" value={isLoading ? "Carregando..." : userData.cep || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="cidade">Cidade: </label>
-              <input disabled id="cidade" type="text" value={userData.cidade || "Não Existe"} />
+              <input disabled id="cidade" type="text" value={isLoading ? "Carregando..." : userData.cidade || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="bairro">Bairro: </label>
-              <input disabled id="bairro" type="text" value={userData.bairro || "Não Existe"} />
+              <input disabled id="bairro" type="text" value={isLoading ? "Carregando..." : userData.bairro || "Não Existe"} />
             </div>
           </fieldset>
 
           <fieldset id="Contatos" className="blocoDados">
             <div className="form-group">
               <label htmlFor="email">Email: </label>
-              <input disabled id="email" type="email" value={userData.email || "Não Existe"} />
+              <input disabled id="email" type="email" value={isLoading ? "Carregando..." : userData.email || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="telefone1">Telefone 1: </label>
-              <input disabled id="telefone1" type="tel" value={userData.telefone1 || "Não Existe"} />
+              <input disabled id="telefone1" type="tel" value={isLoading ? "Carregando..." : userData.telefone1 || "Não Existe"} />
             </div>
             <div className="form-group">
               <label htmlFor="telefone2">Telefone 2: </label>
-              <input disabled id="telefone2" type="tel" value={userData.telefone2 || "Não Existe"} />
+              <input disabled id="telefone2" type="tel" value={isLoading ? "Carregando..." : userData.telefone2 || "Não Existe"} />
             </div>
           </fieldset>
 
